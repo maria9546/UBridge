8164d98e1c519ffcdaad5ca5a87b63b2bf8f55a6
Mariamma <mariathomas310212@gmail.com>
2023-09-06 22:10:06 +0100

    Add a feacture of no result foud

--- Msc project/client/src/pages/Profile.js
+++ Msc project/client/src/pages/Profile.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams, Link } from 'react-router-dom'; 
-
+import { useParams, Link } from 'react-router-dom';
 
 const Profile = () => {
-  const { userId } = useParams(); 
+  const { userId } = useParams();
   const [userDetails, setUserDetails] = useState([]);
   const [selectedCourse, setSelectedCourse] = useState('');
   const [selectedUniversity, setSelectedUniversity] = useState('');
@@ -30,8 +29,7 @@ const Profile = () => {
         console.error('Error fetching universities:', error);
       });
 
-    axios.get('http://localhost:3001/profile/:userId')
-      
+    axios.get(`http://localhost:3001/profile/${userId}`)
       .then(response => {
         setUserDetails(response.data);
         setLoggedUserId(response.data.id);
@@ -47,7 +45,7 @@ const Profile = () => {
     const lowerUsername = user.username ? user.username.toLowerCase() : '';
     const lowerEmail = user.email ? user.email.toLowerCase() : '';
     const lowerName = user.name ? user.name.toLowerCase() : '';
-  
+
     if (selectedCourse && user.course_name !== selectedCourse) {
       return false;
     }
@@ -69,68 +67,71 @@ const Profile = () => {
 
   const loggedInUserName = userDetails.find(user => user.id === Number(userId))?.name;
 
-
   return (
-  <div>
-    <br/><br/><br/><br/><br/>
-  <h1>Welcome {loggedInUserName && ` ${loggedInUserName}`}</h1>
-  <div class="filter-controls">
-  <div class="filter-row">
-    <select
-    className='select-box'
-      value={selectedCourse}
-      onChange={(e) => {
-        setSelectedCourse(e.target.value);
-        setSelectedUniversity('');
-      }}
-    >
-      <option value="">Select Course</option>
-      {courseOptions.map((course) => (
-        <option key={course.id} value={course.name}>
-          {course.name}
-        </option>
-      ))}
-    </select>
-    <select
-    className='select-box'
-      value={selectedUniversity}
-      onChange={(e) => {
-        setSelectedUniversity(e.target.value);
-        setSelectedCourse('');
-      }}
-    >
-      <option value="">Select University</option>
-      {universityOptions.map((university) => (
-        <option key={university.id} value={university.name}>
-          {university.name}
-        </option>
-      ))}
-    </select>
-    <input
-      type="text"
-      placeholder="Search by name or email"
-      className="input-form"
-      value={searchInput}
-      onChange={(e) => setSearchInput(e.target.value)}
-    />
-  </div>
-</div>
-      <div class="user-list">
-        {filteredUserDetails.map(user => (
-          <div key={user.id} class="user">
-            <div class="user-details">
-              <p><b>{user.name}</b></p>
-              <p>{user.email}</p>
-              <p>{user.university_name}</p>
-              <p>{user.course_name}</p>
-              {user.id !== LoggedUserId && user.id !== Number(userId) && (
-              <Link to={`/messages/${userId}/${user.id}`} className="message_button">
-                  Message
-                </Link>
-              )}
+    <div>
+      <br /><br /><br /><br /><br />
+      <h1>Welcome {loggedInUserName && ` ${loggedInUserName}`}</h1>
+      <div className="filter-controls">
+        <div className="filter-row">
+          <select
+            className='select-box'
+            value={selectedCourse}
+            onChange={(e) => {
+              setSelectedCourse(e.target.value);
+              setSelectedUniversity('');
+            }}
+          >
+            <option value="">Select Course</option>
+            {courseOptions.map((course) => (
+              <option key={course.id} value={course.name}>
+                {course.name}
+              </option>
+            ))}
+          </select>
+          <select
+            className='select-box'
+            value={selectedUniversity}
+            onChange={(e) => {
+              setSelectedUniversity(e.target.value);
+              setSelectedCourse('');
+            }}
+          >
+            <option value="">Select University</option>
+            {universityOptions.map((university) => (
+              <option key={university.id} value={university.name}>
+                {university.name}
+              </option>
+            ))}
+          </select>
+          <input
+            type="text"
+            placeholder="Search by name or email"
+            className="input-form"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="user-list">
+        {filteredUserDetails.length === 0 ? (
+          <div className='no-result'>Sorry, No results found. <br/><br/><br/></div>
+        ) : (
+          filteredUserDetails.map(user => (
+            <div key={user.id} className="user">
+              <div className="user-details">
+                <p><b>{user.name}</b></p>
+                <p>{user.email}</p>
+                <p>{user.university_name}</p>
+                <p>{user.course_name}</p>
+                {user.id !== LoggedUserId && user.id !== Number(userId) && (
+                  <Link to={`/messages/${userId}/${user.id}`} className="message_button">
+                    Message
+                  </Link>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
