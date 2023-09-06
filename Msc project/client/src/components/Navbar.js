b77a9743e728c9c140a0da623635fef607f29e22
Mariamma <mariathomas310212@gmail.com>
2023-09-06 19:48:59 +0100

    Make sure when the user logged in,user can acess the home page

--- Msc project/client/src/components/Navbar.js
+++ Msc project/client/src/components/Navbar.js
@@ -1,62 +1,67 @@
-import React from 'react'
-import {Link, useLocation} from 'react-router-dom'
-import Logo from "../img/logo.png"
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Logo from '../img/logo.png';
 
 function Navbar() {
   const location = useLocation();
+  const isLoggedIn = !!localStorage.getItem('userData');
+  const userData = JSON.parse(localStorage.getItem('userData'));
+  const userId = userData ? userData.userId : null;
 
-  const showSignIn = ['/','/blog','/contact','/service'].includes(location.pathname)
-  const showLogout = location.pathname.startsWith('/profile/') || location.pathname.startsWith('/messages/');   
-  const showLogin = ['/register'].includes(location.pathname)
-  const showRegister = ['/login'].includes(location.pathname)
+
+  const handleLogout = () => {
+    localStorage.removeItem('userData');
+  };
 
   return (
-    <div className='navbar'>
-      <div className='container'>
-        <div className='logo'>
-          <img src={Logo} alt=''/>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt='' />
         </div>
-        <div className='links'>
-          <>
-          <Link className='link' to="">
+        <div className="links">
+          <Link className="link" to="/">
             <h6>HOME</h6>
           </Link>
-          <a className="link" href="/service">
+          <Link className="link" to="/service">
             <h6>SERVICES</h6>
-          </a>
-          <Link className='link' to="/blog">
+          </Link>
+          <Link className="link" to="/blog">
             <h6>BLOGS</h6>
           </Link>
-          <Link className='link' to="/contact">
+          <Link className="link" to="/contact">
             <h6>CONTACT</h6>
           </Link>
-          </>
-          {showSignIn && (
-            <Link className='link' to='/register'>
-              <h6>SIGNUP/SIGNIN</h6>
-            </Link>
-          )}
-          {showLogin && (
-            <Link className='link' to='/login'>
-              <h6>LOGIN</h6>
-            </Link>
-          )}
-          {showRegister && (
-            <Link className='link' to='/register'>
-              <h6>REGISTER</h6>
-            </Link>
-          )}
-          {showLogout &&(
-            <Link className='link' to='/logout'>
-              <h6>LOGOUT</h6>
-            </Link>
+          {isLoggedIn ? (
+            <>
+                <Link className="link" to={userId ? `/profile/${userId}` : '/login'}>
+                  <h6>PROFILE</h6>
+                </Link>
+              <Link className="link" to="/logout" onClick={handleLogout}>
+                <h6>LOGOUT</h6>
+              </Link>
+            </>
+          ) : (
+            <>
+              {location.pathname === '/login' ? (
+                <Link className="link" to="/register">
+                  <h6>REGISTER</h6>
+                </Link>
+              ) : location.pathname === '/register' ? (
+                <Link className="link" to="/login">
+                  <h6>LOGIN</h6>
+                </Link>
+              ) : (
+                <Link className="link" to="/register">
+                  <h6>SIGNIN/SIGNOUT</h6>
+                </Link>
+              )}
+            </>
           )}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
-
-       
+export default Navbar;
